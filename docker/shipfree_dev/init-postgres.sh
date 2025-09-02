68c5ad4a53f1c7b0c993aa25e773a9fce99a65b2
RAUL PEDRO MENDEZ GARCIA <raulpedromega@gmail.com>
2025-09-01 19:13:11 -0600

    file cleaning

--- docker/shipfree_dev/init-postgres.sh
+++ docker/shipfree_dev/init-postgres.sh
@@ -16,19 +16,4 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "directus_db" <<-EO
     GRANT ALL PRIVILEGES ON SCHEMA public TO devuser;
     ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO devuser;
     ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO devuser;
-
-    -- Create additional admin user for pgAdmin
-    CREATE USER dbadmin WITH PASSWORD 'admin123' SUPERUSER;
-    GRANT ALL PRIVILEGES ON DATABASE directus_db TO dbadmin;
-    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO dbadmin;
-    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO dbadmin;
-    GRANT ALL PRIVILEGES ON SCHEMA public TO dbadmin;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO dbadmin;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO dbadmin;
 EOSQL
-
-echo "Full superuser privileges granted to devuser"
-echo "Database directus_db created successfully"
-echo "All privileges granted on both databases"
-echo "Additional admin user 'dbadmin' created for pgAdmin access"
-echo "All users can now edit all Directus tables"
